4781908cb994586b6081c08a1ed57b25b4b15bec
Luz <estelarodrigoluz@gmail.com>
2026-04-22 14:41:00 +0100

    idk man

--- include/libft.h
+++ include/libft.h
@@ -34,6 +34,7 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 
 void	ft_bzero(void *s, size_t n);
 void	ft_putnbr_fd(int n, int fd);
+void	ft_putendl_fd(char *s, int fd);
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	*ft_memset(void *s, int c, size_t n);
@@ -46,6 +47,7 @@ char	*ft_strdup(const char *s);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strtrim(char const *s1, char const *set);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
 
